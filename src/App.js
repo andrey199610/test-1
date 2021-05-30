e33b8f8fdbd97da5e6711ee5a571e7bfb0a4f250
Andrey <ignatov199610@gmail.com>
2021-05-30 21:06:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,68 @@
 import React, { useEffect, useState } from "react";
 import { firestore } from "./firebase/config";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
-import "./app.css";
+import "./App.css";
 import Tasks from "./components/tasks";
-
-const onDragEnd = (result, columns, setColumns) => {
-  if (!result.destination) return;
-  const { source, destination } = result;
-  console.log(result.draggableId);
-  if (source.droppableId !== destination.droppableId) {
-    const sourceColumn = columns[source.droppableId];
-    const destColumn = columns[destination.droppableId];
-    const sourceItems = [...sourceColumn.items];
-    const destItems = [...destColumn.items];
-    const [removed] = sourceItems.splice(source.index, 1);
-    destItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...sourceColumn,
-        items: sourceItems,
-      },
-      [destination.droppableId]: {
-        ...destColumn,
-        items: destItems,
-      },
-    });
-  } else {
-    const column = columns[source.droppableId];
-    const copiedItems = [...column.items];
-    const [removed] = copiedItems.splice(source.index, 1);
-    copiedItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...column,
-        items: copiedItems,
-      },
-    });
-  }
-};
 
 function App() {
   const [columns, setColumns] = useState([]);
 
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId];
+      const destColumn = columns[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems,
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems,
+        },
+      });
+
+      fetchupdate(sourceItems, sourceColumn.id);
+      fetchupdate(destItems, destColumn.id);
+    } else {
+      const column = columns[source.droppableId];
+      const copiedItems = [...column.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems,
+        },
+      });
+
+      fetchupdate(copiedItems, column.id);
+    }
+  };
+
   const fetchTasks = async () => {
-    const req = await firestore.collection("groups").get();
+    const req = await firestore.collection("groupstest").get();
     const tempTasks = req.docs.map((task) => ({
       ...task.data(),
       id: task.id,
     }));
     setColumns(tempTasks);
+  };
+
+  const fetchupdate = async (arr, id) => {
+    await firestore.collection("groupstest").doc(id).update({
+      items: arr,
+    });
   };
 
   useEffect(() => {
@@ -59,42 +71,30 @@ function App() {
 
   return (
     <div className="wrapper">
-      <DragDropContext
-        onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
-      >
+      <DragDropContext onDragEnd={onDragEnd}>
         {Object.entries(columns).map(([columnId, column]) => {
           return (
             <div className="column" key={columnId}>
               <h2>{column.name}</h2>
-              <div style={{ margin: 8 }}>
+              <div className="wrapper_droppable">
                 <Droppable droppableId={columnId} key={columnId}>
-                  {(provided, snapshot) => {
-                    return (
-                      <div
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                        style={{
-                          background: snapshot.isDraggingOver
-                            ? "lightblue"
-                            : "lightgrey",
-                          padding: 4,
-                          width: 250,
-                          minHeight: 500,
-                        }}
-                      >
-                        {column.items.map((item, index) => {
-                          return (
-                            <Tasks
-                              item={item}
-                              key={item.id}
-                              index={index}
-                            ></Tasks>
-                          );
-                        })}
-                        {provided.placeholder}
-                      </div>
-                    );
-                  }}
+                  {(provided, snapshot) => (
+                    <div
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                      className="column_tasks"
+                      style={{
+                        background: snapshot.isDraggingOver
+                          ? "lightblue"
+                          : "lightgrey",
+                      }}
+                    >
+                      {column.items.map((item, index) => (
+                        <Tasks item={item} key={item.id} index={index}></Tasks>
+                      ))}
+                      {provided.placeholder}
+                    </div>
+                  )}
                 </Droppable>
               </div>
             </div>
